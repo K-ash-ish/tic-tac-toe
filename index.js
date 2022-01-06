8ef3e310e684a0e77397ac06a141e51854fa00a6
K-ash-ish <kashishsondhiya@gmail.com>
2022-01-06 18:41:38 +0530

    created game grid

---
+++ index.js
@@ -0,0 +1,12 @@
+// const startGame = document.querySelector(".start-btn");
+// const winningSection = document.querySelector(".winning-section");
+
+// startGame.addEventListener('click', ()=>{
+//     winningSection.classList.add("winning-section-show");
+//     setTimeout(() => {
+//         winningSection.classList.remove("winning-section-show")
+//     }, 2000);
+//     console.log(winningSection)
+// })
+
+
